737ce8a25989b0bf5f22735354eedba06d80524e
hashimoti <hashimoti0224@gmail.com>
2026-03-30 20:23:53 +0900

    libの編集GIT

--- vrchat-outfit-manager/src-tauri/src/lib.rs
+++ vrchat-outfit-manager/src-tauri/src/lib.rs
@@ -57,6 +57,7 @@ async fn save_image(base64Data: String, path: String) -> Result<String, String> 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build()) // ★追加
         .plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
